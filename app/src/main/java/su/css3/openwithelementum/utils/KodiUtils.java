2a23cc31691b2a33fff2d436bba5a0889218e42b
Nikolay Vasilchuk <nikolay.vasilchuk@corp.mail.ru>
2018-03-18 22:04:09 +0300

    Find kodi and spmc all versions

--- app/src/main/java/su/css3/openwithelementum/utils/KodiUtils.java
+++ app/src/main/java/su/css3/openwithelementum/utils/KodiUtils.java
@@ -3,29 +3,26 @@ package su.css3.openwithelementum.utils;
 import android.content.Context;
 import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageManager;
-
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.List;
 
 public class KodiUtils {
 
-    private final static Set<String> KODI_PACKAGE_NAMES = new HashSet<>(Arrays.asList(
-            "org.xbmc.kodi",
-            "com.semperpax.spmc16",
-            "com.semperpax.spmc"
-    ));
-
     private static String getKodiPackageName(Context context) {
         PackageManager pm = context.getPackageManager();
-        for (String packageName : KODI_PACKAGE_NAMES) {
-            try {
-                ApplicationInfo ai = pm.getApplicationInfo(packageName, 0);
-                if (ai != null && ai.enabled) {
-                    return packageName;
-                }
-            } catch (PackageManager.NameNotFoundException ignored) {}
+        List<ApplicationInfo> packages = pm.getInstalledApplications(PackageManager.GET_META_DATA);
+
+        for (ApplicationInfo packageInfo : packages) {
+            String packageName = packageInfo.packageName;
+            if (packageName.startsWith("org.xbmc.kodi") || packageName.startsWith("com.semperpax.spmc")) {
+                try {
+                    ApplicationInfo ai = pm.getApplicationInfo(packageName, 0);
+                    if (ai != null && ai.enabled) {
+                        return packageName;
+                    }
+                } catch (PackageManager.NameNotFoundException ignored) {}
+            }
         }
+
         return null;
     }
 
